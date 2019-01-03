bcc0c9ffd5e57567e3d0a96c400dd71d5ae192e3
antao <antao2002@gmail.com>
2019-01-03 13:50:52 +0800

    Modify the Date class test

--- trantor/unittests/DateUnittest.cc
+++ trantor/unittests/DateUnittest.cc
@@ -11,6 +11,8 @@ TEST(Date, constructorTest)
                  trantor::Date(2004, 2, 29).toCustomedFormattedStringLocal("%Y-%m-%d %H:%M:%S", true).c_str());
     EXPECT_STRNE("2001-02-29 00:00:00.000000",
                  trantor::Date(2001, 2, 29).toCustomedFormattedStringLocal("%Y-%m-%d %H:%M:%S", true).c_str());
+    EXPECT_STREQ("2018-01-01 00:00:00.000000",
+                 trantor::Date(2018, 1, 1, 12, 12, 12, 2321).roundDay().toCustomedFormattedStringLocal("%Y-%m-%d %H:%M:%S", true).c_str());
 }
 int main(int argc, char **argv)
 {
